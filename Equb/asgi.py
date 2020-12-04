993e82c457cc78e737b958470f2b70e9987a1791
biruk chafamo <bchafamo@bates.edu>
2020-12-04 17:23:56 -0600

    daphne configurations

--- Equb/asgi.py
+++ Equb/asgi.py
@@ -13,4 +13,4 @@ from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Equb.settings')
 django.setup()
-application = get_default_application
+application = get_default_application()
